296ff741b9e4994f6b7f3cc3688530d6f57eeca2
PolPuigdo <pol.puigdomenech.r@gmail.com>
2019-02-05 02:23:27 +0100

    Components added. Movement script added (currently not working)

---
+++ Not Alone - VR PROJECT/Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Movement : MonoBehaviour
+{
+    public float speed; 
+    private Rigidbody rb2d;
+    public SteamVR_Action_Vector2 touchPadAction;
+
+    void Start()
+    {
+            
+        rb2d = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        Vector2 touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        rb2d.AddForce(touchpadValue * speed);
+    }
+}
