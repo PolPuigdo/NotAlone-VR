8f749f6c2eabbbf5fd33decdea37cfa6ed907ede
PolPuigdo <pol.puigdomenech.r@gmail.com>
2019-02-20 21:31:55 +0100

    Scripts for Picking and Droping objects created and finished

---
+++ Not Alone - VR PROJECT/Assets/Scripts/Hand.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+
+    public SteamVR_Action_Boolean grabAction = null;
+
+    private SteamVR_Behaviour_Pose pose = null;
+    private FixedJoint joint = null;
+
+    private PickDrop currentInteractable = null;
+    private List<PickDrop> interactables = new List<PickDrop>();
+
+    private void Awake()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+        joint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        //Down (Click to grab)
+        if (grabAction.GetLastStateDown(pose.inputSource))
+        {
+            PickUp();
+        }
+
+        //Up (No-Click to drop) GONNA BE CHANGED
+        if (grabAction.GetLastStateUp(pose.inputSource))
+        {
+            Drop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("PickDrop"))
+            return;
+
+        interactables.Add(other.gameObject.GetComponent<PickDrop>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("PickDrop"))
+            return;
+
+        interactables.Remove(other.gameObject.GetComponent<PickDrop>());
+    }
+
+    public void PickUp()
+    {
+        // Get the nearest one
+        currentInteractable = GetNearestObject();
+
+        // Check if it's null
+        if (!currentInteractable)
+            return;
+
+        // Check if it's already held
+        if (currentInteractable.activeHand)
+            currentInteractable.activeHand.Drop();
+
+        // Set the position to the same as the controller
+        currentInteractable.transform.position = transform.position;
+
+        // Atach the object
+        Rigidbody targetBody = currentInteractable.GetComponent<Rigidbody>();
+        joint.connectedBody = targetBody;
+
+        //Set this hand to active
+        currentInteractable.activeHand = this;
+    }
+
+    public void Drop()
+    {
+        // Check if it's null
+        if (!currentInteractable)
+            return;
+
+        // Apply the velocity
+        Rigidbody targetBody = currentInteractable.GetComponent<Rigidbody>();
+        targetBody.velocity = pose.GetVelocity();
+        targetBody.angularVelocity = pose.GetAngularVelocity();
+
+        // Detach nad set variables to null
+        joint.connectedBody = null;
+        currentInteractable.activeHand = null;
+        currentInteractable = null;
+    }
+
+    private PickDrop GetNearestObject()
+    {
+        PickDrop nearest = null;
+        float minDistance = float.MaxValue;
+        float distance = 0.0f;
+
+        foreach(PickDrop interactable in interactables)
+        {
+            distance = (interactable.transform.position - transform.position).sqrMagnitude;
+
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = interactable;
+            }
+        }
+
+        return nearest;
+    }
+}
---
+++ Not Alone - VR PROJECT/Assets/Scripts/TouchPadMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class TouchPadMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    public SteamVR_Action_Vector2 touchPadAction;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        do
+        {
+            //Read the touchpad values
+            Vector2 touchpad = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+            // Move 
+            player.transform.position += (Camera.main.transform.right * touchpad.x + Camera.main.transform.forward * touchpad.y) * Time.deltaTime * 3f; //Movement in x and y
+            player.transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z); //Set the player in the ground
+
+        } while (SteamVR_Actions._default.PressTouchPad.GetStateUp(SteamVR_Input_Sources.Any)); //while the touchpad detects the finger(touch)
+    }
+}
