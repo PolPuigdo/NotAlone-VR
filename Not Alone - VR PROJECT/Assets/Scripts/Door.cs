bbe7944c49d224017eea6dc18391dc0ed5552705
PolPuigdo <pol.puigdomenech.r@gmail.com>
2019-02-22 00:34:55 +0100

    Audio added to the doors (Open,close and locked)

--- Not Alone - VR PROJECT/Assets/Scripts/Door.cs
+++ Not Alone - VR PROJECT/Assets/Scripts/Door.cs
@@ -7,11 +7,14 @@ public class Door : MonoBehaviour
     public bool openable = true;
     private bool isOpen = false;
     private Animator openCloseAnim = null;
+    private AudioSource[] audioDoor;//0-close 1-open 2-locked
+    
 
     // Start is called before the first frame update
     void Start()
     {
         openCloseAnim = GetComponent<Animator>();
+        audioDoor = GetComponents<AudioSource>();
     }
 
     public virtual void DoorAction()
@@ -19,16 +22,16 @@ public class Door : MonoBehaviour
         if (openable && !isOpen) //It can be opened and its closed
         {
             openCloseAnim.SetBool("open", true);
-            //Play audio
+            audioDoor[1].PlayDelayed(0.1f);
             isOpen = true;
         } else if (openable && isOpen) //It can be opened and its open
         {
             openCloseAnim.SetBool("open", false);
-            //Play audio
+            audioDoor[0].PlayDelayed(0.55f);
             isOpen = false;
         } else //It can't be opened
         {
-            //Play sound
+            audioDoor[2].Play();
         }
     }
 
