7e8d54bcde4106679d39182cfdfda78d3b4c84e2
PolPuigdo <pol.puigdomenech.r@gmail.com>
2019-02-21 03:29:59 +0100

    Footsteps added

--- Not Alone - VR PROJECT/Assets/Scripts/TouchPadMovement.cs
+++ Not Alone - VR PROJECT/Assets/Scripts/TouchPadMovement.cs
@@ -5,8 +5,9 @@ using Valve.VR;
 public class TouchPadMovement : MonoBehaviour
 {
     public GameObject player;
-
     public SteamVR_Action_Vector2 touchPadAction;
+
+    private bool isRightFoot = true;
 
     // Executes once per frame
     void Update()
@@ -17,10 +18,10 @@ public class TouchPadMovement : MonoBehaviour
             Vector2 touchpad = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
 
             // Move 
-            player.transform.position += (Camera.main.transform.right * touchpad.x + Camera.main.transform.forward * touchpad.y) * Time.deltaTime * 1.75f; //Movement in x and y
+            player.transform.position += (Camera.main.transform.right * touchpad.x + Camera.main.transform.forward * touchpad.y) * Time.deltaTime * 1.5f; //Movement in x and y
             player.transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z); //Set the player in the ground
 
-            if (!GetComponent<AudioSource>().isPlaying && SteamVR_Actions._default.PressTouchPad.GetStateUp(SteamVR_Input_Sources.Any))
+            if (!GetComponent<AudioSource>().isPlaying && SteamVR_Actions._default.Movement.GetChanged(SteamVR_Input_Sources.Any))
             {
                 PlayFootSteps();
             }
@@ -30,8 +31,21 @@ public class TouchPadMovement : MonoBehaviour
 
     private void PlayFootSteps()
     {
+        //Prioritize the footsteps on the right/left headphone
+        if (isRightFoot)
+        {
+            GetComponent<AudioSource>().panStereo = 0.6f; //Right headphone
+            isRightFoot = false;
+        } else
+        {
+            GetComponent<AudioSource>().panStereo = -0.6f; //Left headphone
+            isRightFoot = true;
+        }
+
+        //Changing the volume and pitch with random numbers so it's more realistic
         GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
-        GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+        GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+        
 
         GetComponent<AudioSource>().Play();
     }
