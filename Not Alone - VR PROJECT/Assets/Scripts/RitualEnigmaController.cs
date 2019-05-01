fd44caa41c9edd4f1b7eeeb86c0f0d7d5b98e9f9
PolPuigdo <pol.puigdomenech.r@gmail.com>
2019-05-01 23:39:50 +0200

    Lighter added and starting to configured it

---
+++ Not Alone - VR PROJECT/Assets/Scripts/RitualEnigmaController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RitualEnigmaController : MonoBehaviour
+{
+    public GameObject[] potPlaces;
+    private int potCont = 0;
+    public Candle[] candles;
+    private int candleCont = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sumPot()
+    {
+        potCont++;
+    }
+
+    public void lessPot()
+    {
+        potCont--;
+    }
+
+    public void sumCandle()
+    {
+        candleCont++;
+    }
+
+    public void checkIfDone()
+    {
+        if (potCont > 3 && candleCont > 4)
+        {
+            //The final Key apears
+        }
+    }
+}
